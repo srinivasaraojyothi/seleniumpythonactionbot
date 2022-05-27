9eca335f51640a4b485a98316e5bf2150ba0dbf1
Jyothi srinivasarao <sjyothi@altimetrik.com>
2022-05-27 21:02:34 +0530

    new structure

---
+++ pyseleniumbot/src/pyseleniumbot.py
@@ -0,0 +1,18 @@
+from selenium.webdriver.common.by import By
+class wrapper:
+    def __init__(self,browser):
+        self.browser=browser
+    def findElementsBy(self,xpath):
+        #self.browser.presence_of_all_elements_located(xpath)
+        #self.browser.visibility_of_element_located(xpath)
+        return self.browser.find_elements(By.XPATH,xpath)
+    def findElementBy(self,xpath):
+        self.browser.presence_of_element_located(xpath)
+        self.browser.visibility_of_element_located(xpath)
+        self.browser.find_element(By.XPATH,xpath)
+    def click(self,xpath):
+        self.browser.visibility_of_element_located(xpath)
+        self.browser.element_to_be_clickable(By.XPATH,xpath) 
+        self.browser.find_element(By.XPATH,xpath).click()
+    def navigateto(self,urlstring):
+        self.browser.get(urlstring)
