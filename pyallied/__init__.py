b7c4982a135fa44521edf99daaab6a00b568aced
Jyothi srinivasarao <sjyothi@altimetrik.com>
2022-08-15 12:14:28 +0530

    modified init file

--- pyallied/__init__.py
+++ pyallied/__init__.py
@@ -1,3 +1,3 @@
-from pyallied.web import alerts,dragAndDrop,DropDown,webElement,webWaits,windowAndFrame
+from pyallied.web import alerts,dragAndDrop,DropDown,webElement,webWaits,windowAndFrame,webElement_v2
 from pyallied.mobile import mobAppium,pollWait,session
 from pyallied.api_crud import crud
