c54757199461c0280000dfa0070fe674e8e37dec
Jyothi srinivasarao <sjyothi@altimetrik.com>
2022-08-31 14:54:36 +0530

    newjsfile

--- pyallied/web/dragDrop.js
+++ pyallied/web/dragDrop.js
@@ -1,44 +1,70 @@
 function simulateDragDrop(sourceNode, destinationNode) {
-    var EVENT_TYPES = {
-        DRAG_END: 'dragend',
-        DRAG_START: 'dragstart',
-        DROP: 'drop'
-    }
+    const DRAG_OVER = 'dragover';
+    const DRAG_END = 'dragend';
+    const DRAG_START = 'dragstart';
+    const DROP = 'drop';
 
-    function createCustomEvent(type) {
-        var event = new CustomEvent("CustomEvent")
-        event.initCustomEvent(type, true, true, null)
+    const createCustomEvent = (type) => {
+        const event = new CustomEvent('CustomEvent');
+        event.initCustomEvent(type, true, true, null);
         event.dataTransfer = {
-            data: {
+            data: {},
+            setData (type, val) {
+                this.data[type] = val;
             },
-            setData: function(type, val) {
-                this.data[type] = val
+            getData (type) {
+                return this.data[type];
             },
-            getData: function(type) {
-                return this.data[type]
-            }
-        }
-        return event
-    }
+            // This ensures the directives 'dnd-moved=""' callback is fired
+            // on the 'dragend' event, removing the original dragged element..
+            dropEffect: 'move'
 
-    function dispatchEvent(node, type, event) {
+        };
+        return event;
+    };
+
+    const dispatchEvent = (node, event) => {
         if (node.dispatchEvent) {
-            return node.dispatchEvent(event)
+            return node.dispatchEvent(event);
         }
-        if (node.fireEvent) {
-            return node.fireEvent("on" + type, event)
+    };
+
+    // dnd-lists >= 2.0.0 uses a custom MIME type of 'application/x-dnd' which it uses to lookup the 'dataTransfer.data' object.
+    // If a custom type is added using the 'dnd-type="'xxxx'"' attribute it is appended to the default type i.e 'application/x-dnd-xxxx'.
+    // We need to add the type here so dnd-list can access the 'dataTransfer.data' object in the drop event handler.
+    const addType = (sourceNode) => {
+        const defaultType = 'application/x-dnd';
+        const customType = sourceNode.getAttribute('dnd-type');
+
+        if (customType) {
+            // Remove single quotes if any present.
+            // dnd-list converts any types provided by 'dnd-type="'xXxX'"' to lowercase, so match that here.
+            const formattedStr = customType.replace(/'/g,'');
+            return [`${defaultType}-${formattedStr.toLowerCase()}`];
         }
-    }
 
-    var event = createCustomEvent(EVENT_TYPES.DRAG_START)
-    dispatchEvent(sourceNode, EVENT_TYPES.DRAG_START, event)
+        return [defaultType];
+    };
 
-    var dropEvent = createCustomEvent(EVENT_TYPES.DROP)
-    dropEvent.dataTransfer = event.dataTransfer
-    dispatchEvent(destinationNode, EVENT_TYPES.DROP, dropEvent)
+    const event = createCustomEvent(DRAG_START);
+    event.dataTransfer.types = addType(sourceNode, destinationNode);
+    dispatchEvent(sourceNode, event);
 
-    var dragEndEvent = createCustomEvent(EVENT_TYPES.DRAG_END)
-    dragEndEvent.dataTransfer = event.dataTransfer
-    dispatchEvent(sourceNode, EVENT_TYPES.DRAG_END, dragEndEvent)
+    const dragOverEvent = createCustomEvent(DRAG_OVER);
+    dragOverEvent.dataTransfer = event.dataTransfer;
+    dispatchEvent(destinationNode, dragOverEvent);
+
+    // When the the first element is dragged over the second element a placeholder
+    // element will appear, this becomes the element the dragged element drops upon.
+    const dropNode = document.querySelector('.dndPlaceholder');
+
+    const dropEvent = createCustomEvent(DROP);
+    dropEvent.dataTransfer = event.dataTransfer;
+    dispatchEvent(dropNode, dropEvent);
+
+    const dragEndEvent = createCustomEvent(DRAG_END);
+    dragEndEvent.dataTransfer = event.dataTransfer;
+    dispatchEvent(sourceNode, dragEndEvent);
+
 }
 simulateDragDrop(arguments[0], arguments[1]);
